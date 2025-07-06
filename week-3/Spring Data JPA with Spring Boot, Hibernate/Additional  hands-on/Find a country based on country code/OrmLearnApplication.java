e0ad90381dd8af855685eb0b092df5108d211440
Nithyasri2426 <146204348+Nithyasri2426@users.noreply.github.com>
2025-07-06 19:44:48 +0530

    Create OrmLearnApplication.java

---
+++ week-3/Spring Data JPA with Spring Boot, Hibernate/Additional  hands-on/Find a country based on country code/OrmLearnApplication.java
@@ -0,0 +1,37 @@
+package com.cognizant.ormlearn;
+
+import com.cognizant.ormlearn.model.Country;
+import com.cognizant.ormlearn.service.CountryService;
+import com.cognizant.ormlearn.service.exception.CountryNotFoundException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class OrmLearnApplication {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrmLearnApplication.class);
+    private static CountryService countryService;
+
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(OrmLearnApplication.class, args);
+        countryService = context.getBean(CountryService.class);
+
+        try {
+            testGetCountryByCode();
+        } catch (CountryNotFoundException e) {
+            LOGGER.error("Exception occurred: {}", e.getMessage());
+        }
+    }
+
+    private static void testGetCountryByCode() throws CountryNotFoundException {
+        LOGGER.info("Start testGetCountryByCode");
+        Country country = countryService.findCountryByCode("IN");
+        LOGGER.debug("Country: {}", country);
+        LOGGER.info("End testGetCountryByCode");
+    }
+}
